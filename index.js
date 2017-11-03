cd99f1feb748144bf312c3c05528bc8a290a88d0
sharmajyo <jyoti.sharma@adslot.com>
2017-11-03 16:21:36 +1100

    unit tests added for index.js

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 inquirer = require('inquirer');
-const {parseUserInput} = require('./util');
-Robot = require('./lib');
+const {parseUserInput, log} = require('./util');
+exports.robot = require('./lib');
 
-const question = {
+exports.startRobot = startRobot = function() {
+  log('Hi there, I am your toy robot. Please place me on the table first and start playing.');
+  log('Use PLACE X,Y,F to place me on a 5 x 5 table. I can be EAST, WEST, NORTH and SOUTH facing.')
+  log('Use MOVE to help me move.')
+  log('Use LEFT or RIGHT to help me change directions.')
+  log('Use REPORT if you want to know where I am.')
+  log('**** All right, lets get going ****')
+  getUserInput();
+}
+
+exports.askQuestion = function () {
+  const question = {
   type: 'input',
   name: 'command',
   message: 'I am waiting for your command now...'
-};
+  };
 
-function startRobot() {
-  console.log('Hi there, I am your toy robot. Please place me on the table first and start playing.');
-  console.log('Use PLACE X,Y,F to place me on a 5 x 5 table. I can be EAST, WEST, NORTH and SOUTH facing.')
-  console.log('Use MOVE to help me move.')
-  console.log('Use LEFT or RIGHT to help me change directions.')
-  console.log('Use REPORT if you want to know where I am.')
-  console.log('**** All right, lets get going ****')
-  ask();
-
+  return inquirer.prompt([question]);
 }
 
-function ask() {
-  inquirer.prompt([question]).then(function (answers) {
-    currentCommand = parseUserInput(answers.command);
+exports.getUserInput = getUserInput = function(keepAsking = true) {
+  exports.askQuestion()
+  .then(function (answers) {
+    userCommand = parseUserInput(answers.command);
 
-    if(currentCommand) {
-      Robot.activate(currentCommand);
+    if(userCommand) {
+      exports.robot.activate(userCommand);
     }
 
-    ask();
+    // we need an option to stop app for the purpose of unit testing
+    if(keepAsking) {
+      getUserInput();
+    }
   });
 }
 
--- test/index.js
+++ test/index.js
@@ -1,10 +1,37 @@
-assert = require('assert');
-robot = require('../main');
+process.env.NODE_ENV = 'test';
 
-describe('toy robot', () => {
+assert = require('assert');
+sinon = require('sinon');
+app = require('../index');
+
+describe('index', () => {
 
   it('should start robot', () => {
-    assert.notEqual(robot, null);
+    assert.notEqual(app.robot, null);
+  });
+
+  describe('getUserInput()', () => {
+
+    const sandbox = sinon.sandbox.create();
+
+    beforeEach(() => {
+      sandbox.stub(app, 'askQuestion').callsFake(() => {
+        return {
+          then: (cb) => {
+            cb({command: 'PLACE 0,0,NORTH'});
+          }
+        };
+      });
+    });
+
+    after(() => {
+      sandbox.restore();
+    });
+
+    it('should receive command from user and move robot accordingly', () => {
+      app.getUserInput(false);
+      assert.deepEqual(app.robot.dimensions, { x: 0, y: 0, facing: 'NORTH' });
+    });
   });
 
 });
